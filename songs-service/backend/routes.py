f7fceaaa5d3e44c0d4c6ebe303a101bcb7df9181
HungThinhNguyen27 <thung4199@gmail.com>
2024-10-30 21:54:46 +0700

    feat: Update docker-compose to connect Flask server and MongoDB

--- songs-service/backend/routes.py
+++ songs-service/backend/routes.py
@@ -5,7 +5,7 @@ import pymongo
 from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
 from pymongo import MongoClient
 from bson import json_util
-from pymongo.errors import OperationFailure
+from pymongo.errors import ConnectionFailure, OperationFailure
 from pymongo.results import InsertOneResult
 from bson.objectid import ObjectId
 import sys
@@ -15,16 +15,36 @@ json_url = os.path.join(SITE_ROOT, "data", "songs.json")
 songs_list: list = json.load(open(json_url))
 
 
-# connect to database
-db_client = MongoClient('localhost', 27017)
+def connect_mongo():
+    try:
+        # connect to database NoSQL
+        client = MongoClient(
+            host='mongodb',
+            port=27017,
+            username='root',
+            password='root123',
+            authSource="admin"
+        )
+        db_client = client["test-connection"]
+        print("Connected to MongoDB successfully!")
+        songs_collection = db_client["songs"]
+        print("Create Songs Collection successfully!")
 
-data = db_client.detals
-# create a collection
-storage = data.songs
+        return db_client, songs_collection
 
-# insert data
-# status = storage.insert_many(songs_list)
-# print(status)
+    except ConnectionFailure as e:
+        print(f"Could not connect to MongoDB: {e}")
+        return None
+
+    except OperationFailure as e:
+        print(f"Authentication failed: {e}")
+        return None
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
+    
+db_client, songs_collection = connect_mongo()
 
 # Delete all documents in the collection
 # storage.delete_many({})
@@ -37,9 +57,14 @@ storage = data.songs
 
 
 
+@app.route("/")
+def insert():
+    songs_collection.insert_many(songs_list)
+    return jsonify(dict(status="OK")), 200
+
+
 def parse_json(data):
     return json.loads(json_util.dumps(data))
-
 
 @app.route("/health")
 def healthz():
@@ -48,19 +73,19 @@ def healthz():
 @app.route("/count")
 def count():
     """return length of data"""
-    count = storage.count_documents({})
+    count = songs_collection.count_documents({})
 
     return {"count": count}, 200
 
 @app.route("/song", methods=["GET"])
 def songs():
-    results = list(storage.find({}))
+    results = list(songs_collection.find({}))
     print(results[0])
     return {"songs": parse_json(results)}, 200
 
 @app.route("/song/<int:id>", methods=["GET"])
 def get_song_by_id(id):
-    song = storage.find_one({"id": id})
+    song = songs_collection.find_one({"id": id})
     if not song:
         return {"message": f"song with id {id} not found"}, 404
     return parse_json(song), 200
@@ -73,13 +98,13 @@ def create_song():
     print(song_in["id"])
 
     # if the id is already there, return 303 with the URL for the resource
-    song = storage.find_one({"id": song_in["id"]})
+    song = songs_collection.find_one({"id": song_in["id"]})
     if song:
         return {
             "Message": f"song with id {song_in['id']} already present"
         }, 302
 
-    insert_id: InsertOneResult = storage.insert_one(song_in)
+    insert_id: InsertOneResult = songs_collection.insert_one(song_in)
 
     return {"inserted id": parse_json(insert_id.inserted_id)}, 201
 
@@ -89,25 +114,29 @@ def update_song(id):
     # get data from the json body
     song_in = request.json
 
-    song = storage.find_one({"id": id})
+    song = songs_collection.find_one({"id": id})
 
     if song == None:
         return {"message": "song not found"}, 404
 
     updated_data = {"$set": song_in}
 
-    result = storage.update_one({"id": id}, updated_data)
+    result = songs_collection.update_one({"id": id}, updated_data)
 
     if result.modified_count == 0:
         return {"message": "song found, but nothing updated"}, 200
     else:
-        return parse_json(storage.find_one({"id": id})), 201
+        return parse_json(songs_collection.find_one({"id": id})), 201
     
 @app.route("/song/<int:id>", methods=["DELETE"])
 def delete_song(id):
 
-    result = storage.delete_one({"id": id})
+    result = songs_collection.delete_one({"id": id})
     if result.deleted_count == 0:
         return {"message": "song not found"}, 404
     else:
         return "", 204
+    
+
+# docker network connect app-network songs-service-flask_app-1
+
